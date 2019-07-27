23f1ad0ab3d61b67ecca9178953c30c156fb0d29
t0mmy93 <43846955+t0mmy93@users.noreply.github.com>
2019-07-27 16:55:21 +0200

    eeee

--- Abschlussaufgabe-EIA2-SS19-master/Abschlussaufgabe/src/js/orders.ts
+++ Abschlussaufgabe-EIA2-SS19-master/Abschlussaufgabe/src/js/orders.ts
@@ -12,9 +12,7 @@ namespace AbschlussaufgabeSS19 {
         document.getElementById("orders-wrapper").innerHTML = "";
 
         let tempJSON: JSON = JSON.parse(response);
-        console.log("%cParsed Response to JSON-Object:", "color: white; background-color: green");
-        console.log(tempJSON);
-
+       
         let datastring: string;
         let orderJSON: JSON;
         let id: string;
@@ -25,14 +23,7 @@ namespace AbschlussaufgabeSS19 {
             id = (decodeURI(tempJSON[key]._id));
             orderIdStorage.push(id);
             datastring = (decodeURI(tempJSON[key].datastring));
-            console.log("datastring for key: " + key);
-            console.log(datastring);
-
             orderJSON = JSON.parse(datastring);
-
-            console.log("%cOrder no." + key + ":", "color: white; background-color: pink");
-            console.log(orderJSON);
-
             newSingleOrder(orderJSON, key);
         }
     }
@@ -51,7 +42,7 @@ namespace AbschlussaufgabeSS19 {
         const divSingleOrder: HTMLElement = newElement("div", "single-order col-12 mb-5", ordersWrapper);
         const divBorder: HTMLElement = newElement("div", "m-2 shadow-lg rounded px-4 py-5", divSingleOrder);
 
-        // HEADER OF ORDER 
+        
 
         const divHeaderRow: HTMLElement = newElement("div", "pb-4 mb-4 px-5 border-bottom border-secondary row", divBorder);
         const divHeadline: HTMLElement = newElement("div", "col-6", divHeaderRow);
@@ -67,12 +58,10 @@ namespace AbschlussaufgabeSS19 {
         closeButton.innerHTML = "✓";
         closeButton.addEventListener("click", function (): void {
             toggleModal("order", headlineText, divSingleOrder, false);
-            console.log(orderIdStorage[parseInt(key)]);
-
-        });
+            });
 
 
-        // ITEMS OF ORDER 
+         
 
         const itemsWrapper: HTMLElement = newElement("div", "items-wrapper", divBorder);
         const divDescriptionRow: HTMLElement = newElement("div", "row pb-3 mx-4", itemsWrapper);
@@ -124,13 +113,8 @@ namespace AbschlussaufgabeSS19 {
 
     function handleStateChangeDeleteOrders(_event: Event): void {
         var xhr: XMLHttpRequest = <XMLHttpRequest>_event.target;
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log("%cServer Response (getData):", "color: white; background-color: blue");
-            console.log("ready: " + xhr.readyState, " | type: " + xhr.responseType, " | status:" + xhr.status, " | text:" + xhr.statusText);
-            console.log("response: " + xhr.response);
-            //getOrdersFromServer();
-        }
-    }
+        if (xhr.readyState == XMLHttpRequest.DONE) 
+              }
 
     export function deleteSingleOrder(elementToRemove: HTMLElement): void {
         const allOrders: HTMLCollection = document.getElementsByClassName("single-order");
@@ -159,10 +143,7 @@ namespace AbschlussaufgabeSS19 {
     function handleStateChangeGetOrders(_event: Event): void {
         var xhr: XMLHttpRequest = <XMLHttpRequest>_event.target;
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log("%cServer Response (getOrders):", "color: white; background-color: blue");
-            console.log("ready: " + xhr.readyState, " | type: " + xhr.responseType, " | status:" + xhr.status, " | text:" + xhr.statusText);
-            console.log("response: " + xhr.response);
-            orderView(xhr.response);
+            
         }
     }
 }
